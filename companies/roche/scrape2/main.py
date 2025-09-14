3dbf31c5842ea4a32b9c364f5d51e7a4e9b16f5a
wol <wolpumba@gmail.com>
2025-09-14 15:08:37 +0000

    configure Chrome WebDriver for web scraping functionality

--- companies/roche/scrape2/main.py
+++ companies/roche/scrape2/main.py
@@ -1,4 +1,16 @@
 from helium import *
+import shutil
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
 
-# go to https://careers.roche.com/de/de/search-results
-start_chrome("https://careers.roche.com/de/de/search-results")
+# configure this browser binary: /usr/bin/google-chrome-stable
+chromedriver_path = shutil.which("chromedriver") or "/usr/bin/chromedriver"
+chrome_binary = shutil.which("google-chrome-stable") or "/usr/bin/google-chrome-stable"
+options = Options()
+options.binary_location = chrome_binary
+service = Service(chromedriver_path)
+driver = webdriver.Chrome(service=service, options=options)
+set_driver(driver)
+
+go_to("https://careers.roche.com/de/de/search-results")
